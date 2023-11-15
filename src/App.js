6121235427ec3bfc726b79703fd5302e5a133763
Sukki <knk951213@naver.com>
2023-11-15 12:26:55 +0900

    feat: calculator-ui, input value 구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React, { useState } from "react";
 function App() {
+  const [calc, setCalc] = useState("0");
+  const getNumber = (_) => {
+    setCalc((prev) => prev + _.target.value);
+  };
+  const clearInput = () => {
+    setCalc("0");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator-wrap">
+      <div className="value-wrap">
+        <input className="value-input" value={calc}></input>
+      </div>
+      <div className="button-wrap">
+        <button onClick={getNumber} value="+">
+          +
+        </button>
+        <button onClick={getNumber} value="-">
+          -
+        </button>
+        <button onClick={getNumber} value="*">
+          *
+        </button>
+        <button onClick={getNumber} value="/">
+          /
+        </button>
+        <button onClick={getNumber} value="7">
+          7
+        </button>
+        <button onClick={getNumber} value="8">
+          8
+        </button>
+        <button onClick={getNumber} value="9">
+          9
+        </button>
+        <button onClick={clearInput} value="C">
+          c
+        </button>
+        <button onClick={getNumber} value="4">
+          4
+        </button>
+        <button onClick={getNumber} value="5">
+          5
+        </button>
+        <button onClick={getNumber} value="6">
+          6
+        </button>
+        <button className="equal-button" onClick={getNumber} value="=">
+          =
+        </button>
+        <button onClick={getNumber} value="1">
+          1
+        </button>
+        <button onClick={getNumber} value="2">
+          2
+        </button>
+        <button onClick={getNumber} value="3">
+          3
+        </button>
+        <button className="zero-button" onClick={getNumber} value="0">
+          0
+        </button>
+        <button onClick={getNumber} value=".">
+          .
+        </button>
+      </div>
     </div>
   );
 }
