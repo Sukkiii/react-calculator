92ac0d1b98747d2712653d1cc63dd942a99a1af2
sukki <knk951213@naver.com>
2023-11-16 16:58:13 +0900

    feat: .point 두 개 찍히는 버그 수정, 동일한 태그 map형태로 입력하기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import "./App.css";
 import React, { useState } from "react";
+
 function App() {
   const [calc, setCalc] = useState("0");
-  const operators = ["/", "*", "+", "-", "."];
+
+  const operators = ["/", "*", "+", "-"];
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   const getNumber = (_) => {
     setCalc((prev) => {
       const currentValue = _.target.value;
       const lastChar = prev.charAt(prev.length - 1);
+
+      if (currentValue === "." && prev.includes(".")) {
+        return prev;
+      }
 
       if (operators.includes(lastChar) && operators.includes(currentValue))
         return prev;
@@ -14,13 +22,16 @@ function App() {
       return prev === "0" ? _.target.value : prev + _.target.value;
     });
   };
+
   const deleteBtn = () => {
     let newStr = calc.slice(0, -1);
     setCalc((prev) => newStr);
   };
+
   const clearInput = () => {
     setCalc("0");
   };
+
   const calculateInput = () => {
     setCalc((prev) => eval(prev).toString());
   };
@@ -31,59 +42,34 @@ function App() {
         <input className="value-input" value={calc}></input>
       </div>
       <div className="button-wrap">
-        <button onClick={getNumber} value="+">
-          +
-        </button>
-        <button onClick={getNumber} value="-">
-          -
-        </button>
-        <button onClick={getNumber} value="*">
-          *
-        </button>
-        <button onClick={getNumber} value="/">
-          /
-        </button>
-        <button onClick={getNumber} value="7">
-          7
-        </button>
-        <button onClick={getNumber} value="8">
-          8
-        </button>
-        <button onClick={getNumber} value="9">
-          9
-        </button>
-        <button onClick={clearInput} value="C">
+        {operators.map((operator, i) => {
+          return (
+            <button key={i} onClick={getNumber} value={operator}>
+              {operator}
+            </button>
+          );
+        })}
+        <button className="clear-button" onClick={clearInput} value="C">
           C
-        </button>
-        <button onClick={getNumber} value="4">
-          4
-        </button>
-        <button onClick={getNumber} value="5">
-          5
-        </button>
-        <button onClick={getNumber} value="6">
-          6
-        </button>
-        <button onClick={getNumber} value="1">
-          1
-        </button>
-        <button onClick={getNumber} value="2">
-          2
-        </button>
-        <button onClick={getNumber} value="3">
-          3
         </button>
         <button className="zero-button" onClick={getNumber} value="0">
           0
         </button>
-        <button onClick={getNumber} value=".">
-          .
-        </button>
         <button className="equal-button" onClick={calculateInput} value="=">
           =
         </button>
-        <button className="deleteBtn" onClick={deleteBtn}>
+        <button className="delete-button" onClick={deleteBtn}>
           del
+        </button>
+        {numbers.map((number, i) => {
+          return (
+            <button key={i} onClick={getNumber} value={number}>
+              {number}
+            </button>
+          );
+        })}
+        <button onClick={getNumber} value=".">
+          .
         </button>
       </div>
     </div>
